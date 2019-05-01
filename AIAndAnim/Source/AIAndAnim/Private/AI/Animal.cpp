a9e3cd48d8aff857962b053430445265e820e3f5
mosipa <manisiek@gmail.com>
2019-05-01 20:24:14 +0200

    AAA07 added Animal class - added HealthComponent setter and binded event OnDeath; reparented BP_Animal - parent class is Animal

---
+++ AIAndAnim/Source/AIAndAnim/Private/AI/Animal.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Animal.h"
+#include "HealthComponent.h"
+
+// Sets default values
+AAnimal::AAnimal()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAnimal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	HealthComponent->OnDeath.AddDynamic(this, &AAnimal::OnDeath);
+}
+
+// Called every frame
+void AAnimal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAnimal::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AAnimal::SetHealthComponent(UHealthComponent* NewComponent)
+{
+	HealthComponent = NewComponent;
+}
+
+void AAnimal::OnDeath()
+{
+	HealthComponent->DestroyComponent();
+	
+	HealthComponent = nullptr;
+}
